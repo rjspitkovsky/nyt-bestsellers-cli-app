73d321c43743c3c80a41b74c71fa27aafbcbcef0
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-05 17:27:21 -0500

    work on pick_category method

--- lib/nyt_bestsellers_cli_gem/CLI.rb
+++ lib/nyt_bestsellers_cli_gem/CLI.rb
@@ -3,6 +3,7 @@ class CLI
   def call
     Scraper.new.scrape
     greeting
+    pick_category
   end
 
   def greeting
@@ -10,7 +11,19 @@ class CLI
     Category.all.each.with_index(1) do |section_title, i|
       puts "#{i}. #{section_title.name}"
     end
-    puts "Please choose a category:"
+    #puts "Please choose a category:"
   end
+
+  def pick_category
+    puts "Please choose the number of the category you wish to inspect:"
+    input = gets.strip
+    if input.to_i > 0 && input.to_i < 6
+       Category.all[input.to_i - 1].books.each.with_index(1) do |book, i|
+         puts "#{i}. #{book.title} - #{book.author}"
+       end 
+    end
+  end
+
+
 
 end
