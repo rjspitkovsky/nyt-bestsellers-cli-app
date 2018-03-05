0cfc6aeb1e4e90f56967ebc50fecc03d8b884160
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-05 15:12:18 -0500

    add code to scraper.rb

--- lib/nyt_bestsellers_cli_gem/scraper.rb
+++ lib/nyt_bestsellers_cli_gem/scraper.rb
@@ -1,3 +1,14 @@
 class Scraper
 
-end 
+  attr_accessor :doc
+
+  def initialize(url)
+    @doc = Nokogiri::HTML(open(url))
+  end
+
+  def scrape_category_name
+    @doc.css("header.subcategory-header h2.subcategory_heading a").attr("href").text
+  end 
+
+
+end
