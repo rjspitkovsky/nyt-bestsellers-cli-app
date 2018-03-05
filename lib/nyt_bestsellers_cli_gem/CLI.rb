4bd0ed919965ee5d6f8a4346da6bf551a3ea2372
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-05 18:58:26 -0500

    cli.rb getting way too complicated: pick_category, book_details methods

--- lib/nyt_bestsellers_cli_gem/CLI.rb
+++ lib/nyt_bestsellers_cli_gem/CLI.rb
@@ -5,17 +5,21 @@ class CLI
     greeting
     pick_category
     book_details
+    goodbye!
   end
 
   def greeting
     puts "Welcome to the New York Times Bestseller List!"
-    Category.all.each.with_index(1) do |section_title, i|
-      puts "#{i}. #{section_title.name}"
-    end
+    # Category.all.each.with_index(1) do |section_title, i|
+    #   puts "#{i}. #{section_title.name}"
+    # end
     #puts "Please choose a category:"
   end
 
   def pick_category
+    Category.all.each.with_index(1) do |section_title, i|
+      puts "#{i}. #{section_title.name}"
+    end
     puts "Please choose the number of the category you wish to inspect:"
     input = gets.strip
     if input.to_i > 0 && input.to_i < 6
@@ -24,30 +28,72 @@ class CLI
        end
     else
       pick_category
+      book_details
+      goodbye!
   end
 end
 
   def book_details
     puts "Please type in the title of the book to see its summary."
     puts "Type back to review other categories."
+      #while input != "back" || input != "exit"
       input = gets.strip.upcase
       if Book.find_by_title(input) != nil
         if Book.find_by_title(input).summary != ""
           puts Book.find_by_title(input).summary
+          puts "Would you like to buy this book? (y/n)"
+            input = gets.strip.downcase
+              if input == "y"
+                system("open https://www.nytimes.com/books/best-sellers/")
+              else
+                greeting
+                pick_category
+                book_details
+                goodbye!
+              end
+
+
+          #puts "Would you like to see another books summary? (y/n)"
+          #pick_another_book
+
         else
           puts "Sorry, this book does not have a summary provided."
+          #pick_another_book
+          #break
+
+        end
+
+      else
+      pick_category
       end
-      end
+
+      #pick_category
+    #end
   end
 
-  
 
+# def pick_another_book
+#   input = ""
+#   puts "Would you like to see another books' summary? (y/n)"
+#   until input != "exit"
+#   input = gets.strip
+#   if input.upcase == "Y" || "YES"
+#     pick_category
+#     book_details
+#   else
+#     goodbye!
+#   end
+# end
+# end
+#
+#
   def goodbye!
-    input = gets.strip
-    if input == "exit"
+    # input = ""
+    # input = gets.strip
+    # if input == "exit"
     puts "Thank you for stopping by!"
   end
-  end
+  #end
 
 
 
