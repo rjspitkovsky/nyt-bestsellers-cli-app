c9ab80c6b3cf94c668de666d21992c2728f429b0
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-05 16:11:13 -0500

    work on scrape_category_name method

--- lib/nyt_bestsellers_cli_gem/scraper.rb
+++ lib/nyt_bestsellers_cli_gem/scraper.rb
@@ -1,14 +1,21 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Scraper
 
   attr_accessor :doc
 
-  def initialize(url)
-    @doc = Nokogiri::HTML(open(url))
+  def initialize
+    @doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
   end
 
   def scrape_category_name
-    @doc.css("header.subcategory-header h2.subcategory_heading a").attr("href").text
-  end 
+    @doc.css("section.subcategory").each do |section|
+      name = section.css("a.subcategory-heading-link").text
+      Category.new(name)
+    end 
+  end
 
 
 end
