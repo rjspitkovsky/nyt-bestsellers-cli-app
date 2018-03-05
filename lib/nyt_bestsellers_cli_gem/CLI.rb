5b1f626def6f1c41b1b4f2513f5dba18ef690364
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-05 17:15:53 -0500

    work on greeting method

--- lib/nyt_bestsellers_cli_gem/CLI.rb
+++ lib/nyt_bestsellers_cli_gem/CLI.rb
@@ -1,6 +1,16 @@
 class CLI
 
   def call
-    puts "testing environment"
+    Scraper.new.scrape
+    greeting
   end
+
+  def greeting
+    puts "Welcome to the New York Times Bestseller List!"
+    Category.all.each.with_index(1) do |section_title, i|
+      puts "#{i}. #{section_title.name}"
+    end
+    puts "Please choose a category:"
+  end
+
 end
